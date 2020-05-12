62c7b09dab470fb13ed6da08f80f1ef29fdf6585
AhmedAGadir <ahmed.gadir@gmail.com>
2020-05-12 17:05:41 +0100

    update

--- src/App.js
+++ src/App.js
@@ -1,26 +1,132 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import { AgGridReact } from 'ag-grid-react';
+import 'ag-grid-enterprise';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+import 'ag-grid-community/dist/styles/ag-grid.css';
+import 'ag-grid-community/dist/styles/ag-theme-alpine-dark.css';
+
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      columnDefs: [
+        {
+          field: 'athlete',
+          minWidth: 220,
+        },
+        {
+          field: 'country',
+          minWidth: 200,
+        },
+        { field: 'year' },
+        {
+          field: 'sport',
+          minWidth: 200,
+        },
+        { field: 'gold' },
+        { field: 'silver' },
+        { field: 'bronze' },
+      ],
+      defaultColDef: {
+        flex: 1,
+        minWidth: 100,
+      },
+      rowModelType: 'serverSide',
+    };
+  }
+
+  onGridReady = params => {
+    this.gridApi = params.api;
+    this.gridColumnApi = params.columnApi;
+
+    this.initializeData();
+
+    // setTimeout(() => {
+    //   var that = this;
+    //   debugger;
+    // }, 2000);
+  };
+
+  initializeData = () => {
+    console.log('initializeData')
+    const httpRequest = new XMLHttpRequest();
+    const updateData = data => {
+      var fakeServer = createFakeServer(data);
+      var datasource = createServerSideDatasource(fakeServer);
+      this.gridApi.setServerSideDatasource(datasource);
+
+    };
+
+    httpRequest.open(
+      'GET',
+      'https://raw.githubusercontent.com/ag-grid/ag-grid/master/grid-packages/ag-grid-docs/src/olympicWinners.json'
+    );
+    httpRequest.send();
+    httpRequest.onreadystatechange = () => {
+      if (httpRequest.readyState === 4 && httpRequest.status === 200) {
+        updateData(JSON.parse(httpRequest.responseText));
+      }
+    };
+  }
+
+  updateData = () => {
+    this.initializeData()
+  }
+
+  render() {
+    return (
+      <div style={{ width: '100%', height: '100vh' }}>
+        <div
+          id="myGrid"
+          style={{
+            height: '100%',
+            width: '100%',
+          }}
+          className="ag-theme-alpine-dark"
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+          <AgGridReact
+            columnDefs={this.state.columnDefs}
+            defaultColDef={this.state.defaultColDef}
+            rowModelType={this.state.rowModelType}
+            onGridReady={this.onGridReady}
+          />
+        </div>
+      </div>
+    );
+  }
 }
 
-export default App;
+function createServerSideDatasource(server) {
+  return {
+    getRows: function (params) {
+      console.log('[Datasource] - rows requested by grid: ', params.request);
+      var response = server.getData(params.request);
+      setTimeout(function () {
+        if (response.success) {
+          params.successCallback(response.rows, response.lastRow);
+        } else {
+          params.failCallback();
+        }
+      }, 500);
+    },
+  };
+}
+function createFakeServer(allData) {
+  return {
+    getData: function (request) {
+      var requestedRows = allData.slice(request.startRow, request.endRow);
+      var lastRow = getLastRowIndex(request, requestedRows);
+      return {
+        success: true,
+        rows: requestedRows,
+        lastRow: lastRow,
+      };
+    },
+  };
+}
+function getLastRowIndex(request, results) {
+  if (!results) return undefined;
+  var currentLastRow = request.startRow + results.length;
+  return currentLastRow < request.endRow ? currentLastRow : undefined;
+}
--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,79 @@
 import React from 'react';
-import { render } from '@testing-library/react';
 import App from './App';
+import { AgGridReact } from 'ag-grid-react';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+import { mount } from 'enzyme';
+
+let component = null;
+let agGridReact = null;
+
+// const ensureGridApiHasBeenSet = (component) => {
+//   return new Promise(function (resolve, reject) {
+//     (function waitForGridReady() {
+
+//       if (component.instance().api) {
+//         return resolve();
+//       }
+
+//       setTimeout(waitForGridReady, 10);
+//     })();
+//   });
+// };
+
+beforeEach((done) => {
+  // ignore license errors 
+  jest.spyOn(console, 'error').mockImplementation(() => { });
+
+  // component = mount(<App />);
+  // agGridReact = component.find(AgGridReact).instance();
+  // ensureGridApiHasBeenSet(component).then(() => done());
+  done();
+});
+
+afterEach(() => {
+  component.unmount();
+  agGridReact = null;
+})
+
+it('renders rows when response is successful', (done) => {
+
+  let dummyRow = [{
+    athlete: 'Michael Phelps',
+  }];
+
+  component = mount(<App />);
+
+  component.instance().initializeData = jest.fn(initData)
+
+  function initData(params) {
+    console.log('initData')
+    let successDataSource = {
+      getRows: params => {
+        params.successCallback([dummyRow], 1);
+      }
+    }
+    component.find(AgGridReact).instance().api.setServerSideDatasource(successDataSource);
+  }
+
+  component.update();
+
+  // component.instance().updateData();
+
+  setTimeout(() => {
+    let firstNode = component.instance().gridApi.getDisplayedRowAtIndex(0)
+    expect(firstNode.data.athlete).toEqual(dummyRow.athlete);
+    done();
+  }, 2000);
+
+});
+
+it('fails gracefully when response fails', () => {
+  component = mount(<App />);
+  // let failDataSource = {
+  //   getRows: params => {
+  //     params.failCallback();
+  //   }
+  // };
+
+  // params.api.setServerSideDatasource(failDataSource);
 });
